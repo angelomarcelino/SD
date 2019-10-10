ad89fe9601c048c497d77680a34f9fe6135786a3
angelomarcelino <amc.marcelino.cordeiro@gmail.com>
2019-10-10 12:36:24 -0300

    Tudo menos timer

--- carProject/src/main.cpp
+++ carProject/src/main.cpp
@@ -1,22 +1,83 @@
 #include <avr/interrupt.h>
 #include <Arduino.h>
 
+#define F_CPU 16000000UL
+
 ISR(TIMER1_COMPA_VECT) {
-	// TODO
+	Serial.println("Interrupt");
 }
 
 void blinkRightLight() {
-	Serial.println("Pisca direito\n");
+	Serial.println("Pisca direito");
 }
 
 void blinkLeftLight() {
-	Serial.println("Pisca esquerdo\n");	
+	Serial.println("Pisca esquerdo");	
+}
+
+void modoDesligado() {
+	// Serial.print("\n\t____Modo Desligado____\n");
+
+	// Zerar contadores 
+
+	// Desligar os LEDs/Farois
+	PORTD &= 0b00000000;
+	PORTD &= 0b00000000;
+}
+
+void modoFarois() {
+	Serial.print("\n\t____Modo Farois____\n");
+	// Ligar farois 
+	PORTD |= 0b00010000;
+	PORTD |= 0b00100000;
+
+	// Permite alterar o estado dos piscas
+	if (!(PINB & 0b00000100))
+		blinkRightLight();
+		
+	if (!(PINB & 0b00001000))
+		blinkLeftLight();
+
+	// Sensor de combustivel
+	ADMUX |= 0b00000010;
+	ADCSRA |= 0b01000000;
+	while (!(ADCSRA & 0b00010000));
+	
+	Serial.print("\nSensor de combustivel: ");
+	Serial.println(ADCH);
+
+}
+
+void modoLigado() {
+	Serial.print("\n\t____Modo Ligado____\n");
+
+	// Enviar um sinal com 1s de duração
+
+	// Ler velocidade a cada 100ms
+	// Sensor de velocidade
+	ADMUX |= 0b00000001;
+	ADCSRA |= 0b01000000;
+	while (!(ADCSRA & 0b00010000));
+	
+	Serial.print("\nSensor de velocidade: ");
+	Serial.println(ADCH);
+
+	// Ligar farois 
+	PORTD |= 0b00010000;
+	PORTD |= 0b00100000;
+
+	// Permite alterar o estado dos piscas
+	if (!(PINB & 0b00000100))
+		blinkRightLight();
+		
+	if (!(PINB & 0b00001000))
+		blinkLeftLight();
 }
 
 int main(void) {
 	Serial.begin(115200);
+	Serial.println("Ready");
 
-	uint8_t valor_AD;
 	// Configuração do ADC
 	ADMUX |= 0b01100000;  // AVCC como referência; ADLAR = 1; MUX = 0000
 	ADCSRA |= 0b10000111; // Enable do ADC, Escalonador de Clock em 128;
@@ -29,43 +90,42 @@ int main(void) {
 	PORTD |= 0b00000000; // Nenhum PULL-UP
 
 	//Configuração do Contador
-	DDRB   |= 0b00000111;
-	TCCR1A |= 0b10000011;
-	TCCR1B |= 0b00001001;
-	TIMSK1 |= 0b00000010;
+	//  Timer/Counter Control Register
+	// TCCR0A |= 0b10000011;
+	// Timer/Counter Register 
+	// 	TCCR0B |= 0b00001001;
+	// Timer/Counter Interrupt Mask Register
+	// TIMSK0 |= 0b00000010;
+
+	//Configuração do Contador
+	// DDRB   |= 0b00000100;
+	// TCCR1A |= 0b10000011;
+	// TCCR1B |= 0b00001011;
+	// TIMSK1 |= 0b00000010;
 
 	// Habilitação dos interrupts
 	// sei();
 
+	OCR2A = 62;
+    TCCR2A |= (1 << WGM21);
+    // Set to CTC Mode
+    TIMSK2 |= (1 << OCIE2A);
+    //Set interrupt on compare match
+    TCCR2B |= (1 << CS21);
+    // set prescaler to 64 and starts PWM
+    // sei();
+
 	while(true) {
-		if (!(PINB & 0b00000100))
-			blinkRightLight();
-			
-		if (!(PINB & 0b00001000))
-			blinkLeftLight();;
-		
-		// PORTD |= 0b00010000;
-		// PORTD |= 0b00100000;
-
-
-		// Começa a conversão AD
+		// Lê a chave de ignição
 		ADMUX &= 0b11110000;
 		ADCSRA |= 0b01000000;
 		while (!(ADCSRA & 0b00010000));
-		valor_AD = ADCH;
 		
-		Serial.print("Valor ADC0: ");
-		Serial.println(valor_AD); 
-
-		ADMUX |= 0b00000001;
-		ADCSRA |= 0b01000000;
-		while (!(ADCSRA & 0b00010000));
-		valor_AD = ADCH;
-		
-		Serial.print("Valor ADC1: ");
-		Serial.println(valor_AD);
-		Serial.println();
-
-		OCR1A = ADC;
+		if (ADCH < 50)
+			modoDesligado();
+		else if (ADCH > 50 and ADCH < 100) 
+			modoFarois(); 
+		else 
+			modoLigado();
 	}
 }
