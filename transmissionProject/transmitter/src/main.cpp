c8c1e7670952943a1963ba6b65d4d1eddf261f27
vanluwin <richardsonsantiago@ufrn.edu.br>
2019-11-28 17:50:09 -0300

    transmitter

--- transmissionProject/transmitter/src/main.cpp
+++ transmissionProject/transmitter/src/main.cpp
@@ -1,27 +1,26 @@
 #include <Arduino.h>
+#include <LiquidCrystal.h>
 #include <util/delay.h>
-#include "Display.h"
+// #include "Display.h"
 
 // Transmit rate in bps
 #define TX_RATE 5
+#define LCD_RS 6
+#define LCD_EN 7
+#define LCD_D4 2
+#define LCD_D5 3
+#define LCD_D6 4
+#define LCD_D7 5
 
-const char *message = "Hello, world!";
-
-void myDelay(uint16_t dly) {
-    // Define o valor alvo para 62499
-    OCR1A = TCNT1 + dly - 1;
-
-    // Define o presclaer do timer1 como 256
-    TCCR1B |= (1 << WGM12) | (1 << CS12);
-
-    // Espera pelo evento de overflow do contador 
-    while ((TIFR1 & (1 <<  OCF1A)) == 0);
-
-    // Limpa a flag
-    TIFR1 |= (1<<OCF1A);  
-}
+const char *message = "Vai dar certo !!";
 
 int main(void) {
+
+    // Incia o LCD
+    LiquidCrystal lcd(LCD_RS, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
+    lcd.begin(16, 2);
+    lcd.setCursor(0, 0);
+    lcd.print(message);
 
     /*
         TX_CLOCK na porta D8
@@ -30,27 +29,37 @@ int main(void) {
     DDRB  |= 0b00000011;
     PORTB |= 0b00000000;
 
-    // Incia o LCD
-    lcdBegin();
-
     for (uint8_t byte_idx = 0; byte_idx < strlen(message); byte_idx++) {
         char tx_byte = message[byte_idx];
+
+        // Limpa a segunda do display
+        lcd.noCursor();
+        lcd.setCursor(0, 1);
+        lcd.print("        ");
+        lcd.setCursor(byte_idx, 0);
+        lcd.cursor();
 
         for (uint8_t bit_idx = 0; bit_idx < 8; bit_idx++) {
             bool tx_bit = tx_byte & (0x80 >> bit_idx);    
 
+            // Envia o bit
             if(tx_bit)
                 PORTB |= 0b00000010;
             else
                 PORTB &= 0b01111101;
 
             _delay_ms((1000 / TX_RATE) / 2);
-            // myDelay((1000 / TX_RATE) / 2);
+
+            // Atualiza o LCD
+            lcd.noCursor();
+            lcd.setCursor(bit_idx, 1);
+            lcd.print(tx_bit ? "1" : "0");
+            lcd.setCursor(byte_idx, 0);
+            lcd.cursor();
 
             // Pulsa o clock
             PORTB |= 0b00000001;
             _delay_ms((1000 / TX_RATE) / 2);
-            // myDelay((1000 / TX_RATE) / 2);
             PORTB &= 0b01111110;
         }
     }
